45d07f3fcac9546a36bb67606575b0c3f7792b52
soukaina taleb <soukainataleb79@gmail.com>
2025-12-04 15:39:36 +0100

    i just added hero section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Navbar} from './navbar/navbar';
+import {Section} from './section/section';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar ,Section],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
