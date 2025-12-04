2861728bea16018788d02e57f43bfd591cbeb8d4
soukaina taleb <soukainataleb79@gmail.com>
2025-12-04 16:02:14 +0100

    i just added About Section

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Navbar} from './navbar/navbar';
 import {Section} from './section/section';
+import {About} from './about/about';  
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar ,Section],
+  imports: [RouterOutlet, Navbar ,Section, About],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
